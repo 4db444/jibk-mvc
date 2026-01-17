484339f8d6e590cc708f73367f2a37bd929682a9
4db444 <brahim77990alhyan@gmail.com>
2026-01-17 23:03:57 +0100

    craete database class(singletone) with the config file)

---
+++ App/Core/Database.php
@@ -0,0 +1,20 @@
+<?php
+    namespace App\Core;
+
+    use PDO;
+
+    class Database {
+        private static ?Database $instance = NULL;
+        private ?PDO $connection = NULL;
+
+        private function __construct () {
+            $this->connection = new PDO(DATABASE . ":host=" . DATABASE_HOST . ";dbname=" . DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
+        }
+
+        public static function get_instance(){
+            if (!self::$instance) self::$instance = new self();
+            return self::$instance->connection;
+        }
+    }
+
+    Database::get_instance();
---
+++ Config/database.example.php
@@ -0,0 +1,8 @@
+<?php
+    const DATABASE = "mysql";               // your database driver
+    const DATABASE_NAME = "dbname";         // your database name
+    const DATABASE_USER = "root";           // your database user
+    const DATABASE_HOST = "127.0.0.1";      // your database host
+    const DATABASE_PASSWORD = "secret";     // your database password
+
+    // change the file name  from database.example.php ----> database.php
